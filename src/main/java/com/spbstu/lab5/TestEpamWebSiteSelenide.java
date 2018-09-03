56bfe84035dd43342f7e9735bfb9e230f2645431
MksatBek <36085274+MksatBek@users.noreply.github.com>
2018-09-03 17:48:46 +0300

    Update TestEpamWebSiteSelenide.java

--- src/main/java/com/spbstu/lab5/TestEpamWebSiteSelenide.java
+++ src/main/java/com/spbstu/lab5/TestEpamWebSiteSelenide.java
@@ -5,10 +5,13 @@ import com.epam.jdi.uitests.web.selenium.elements.common.Label;
 import com.epam.jdi.uitests.web.selenium.elements.composite.WebSite;
 import com.epam.jdi.uitests.web.selenium.elements.pageobjects.annotations.JSite;
 import com.spbstu.lab5.entities.User;
+import com.spbstu.lab5.entities.Users;
+import com.spbstu.lab5.enums.PAGE_DATA;
 import com.spbstu.lab5.pages.HomePageJDI;
 import com.spbstu.lab5.pages.MetalsAndColorsPage;
 import org.openqa.selenium.support.FindBy;
 
+import static com.spbstu.lab5.entities.Users.PETER;
 
 @JSite("https://jdi-framework.github.io/tests/")
 public class TestEpamWebSiteSelenide extends WebSite {
@@ -27,10 +30,13 @@ public class TestEpamWebSiteSelenide extends WebSite {
     @FindBy(css = ".uui-navigation | [href=\"page2.htm\"]")
     public static Button metalsAndColorsPageButton;
 
-    public static void login(String name, String password) {
+    public static void login(User user1) {
         profilePhoto.click();
-        loginForm.loginAs(new User(name, password));
+        loginForm.loginAs(user1);
     }
+    //public static void login(){
+    //    login(PAGE_DATA.LOGIN.str, PAGE_DATA.PASSWORD.str);
+    //}
 
     public static void openMetalsAndColorsPage() {
         metalsAndColorsPageButton.click();
